1708e683b21f0a08924e1747bef2256e85d83ef6
Vesko Kenashkov <vesko@azonmedia.com>
2020-01-17 16:22:39 +0200

    wip

--- app/src/Controllers/Admin.php
+++ app/src/Controllers/Admin.php
@@ -1,14 +1,26 @@
 <?php
+declare(strict_types=1);
 
 namespace GuzabaPlatform\Crud\Controllers;
 
-use Guzaba2\Mvc\ActiveRecordController;
+use Guzaba2\Http\Method;
+use GuzabaPlatform\Platform\Application\BaseController;
 use Psr\Http\Message\ResponseInterface;
 
-class Admin extends ActiveRecordController
+class Admin extends BaseController
 {
+    protected const CONFIG_DEFAULTS = [
+        'routes'        => [
+            '/admin/crud'               => [
+                Method::HTTP_GET              => [self::class, 'main']
+            ],
+        ],
+    ];
+
+    protected const CONFIG_RUNTIME = [];
+
     public function main() : ResponseInterface
     {
-        
+
     }
 }
